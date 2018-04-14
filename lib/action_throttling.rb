b6dc5406a356293d9e07c98315cab042d1a1720e
Emil Kampp <emil@kampp.me>
2018-04-14 11:59:36 +0200

    Add regeneration specs

--- lib/action_throttling.rb
+++ lib/action_throttling.rb
@@ -9,14 +9,38 @@ module ActionThrottling
 
   module InstanceMethods
     def cost(price)
+      # If last time the bucket was called is older than the regeneration limit
+      # regenerate the bucket.
+      regenerate if last_called < regeneration_time
+
       # Deduct the price from the bucket. If it's below zero we raiss a
       # HttpError::ToManyRequests exception/
       if redis.decrby(bucket_key, price) < 0
         raise HttpError::ToManyRequests, "Throttling enabled. Please try again later"
       end
+
+      # Store when the cost was last called so we can calculate regeneration
+      redis.set last_call_key, Time.now.httpdate
     end
 
     private
+
+      def last_call_key
+        "#{bucket_key}-last-call"
+      end
+
+      def regenerate
+        redis.set bucket_key, ActionThrottling.configuration.regenerate_amount
+      end
+
+      def last_called
+        value = redis.get(last_call_key).presence
+
+        # trigger regeneration when first cost is first called
+        value ||= (regeneration_time - 1.second).httpdate
+
+        Time.parse value
+      end
 
       def bucket_key
         if ActionThrottling.configuration.bucket_key.is_a? Proc
@@ -24,6 +48,10 @@ module ActionThrottling
         else
           ActionThrottling.configuration.bucket_key
         end
+      end
+
+      def regeneration_time
+        ActionThrottling.configuration.regenerate_interval.ago
       end
 
       def redis
--- spec/rails/action_throttling_spec.rb
+++ spec/rails/action_throttling_spec.rb
@@ -9,20 +9,26 @@ class ActionThrottlingTest
 end
 
 RSpec.describe ActionThrottlingTest do
+  let(:instance) { described_class.new }
+
   describe '#cost' do
-    subject { described_class.new.cost(value) }
+    subject { instance.cost(value) }
     let(:value) { 10 }
 
     it 'raises HttpError::ToManyRequests if no tokens left in bucket' do
+      Redis.new.set ActionThrottling.configuration.bucket_key, 9
       expect { subject }.to raise_error HttpError::ToManyRequests
     end
 
-    context 'with tokens in bucket' do
-      before { Redis.new.set(ActionThrottling.configuration.bucket_key, 11) }
+    it 'does not raise anything when sufficient tokens left' do
+      Redis.new.set ActionThrottling.configuration.bucket_key, 11
+      expect { subject }.not_to raise_error HttpError::ToManyRequests
+    end
 
-      it 'raises HttpError::ToManyRequests if no tokens left in bucket' do
-        expect { subject }.not_to raise_error HttpError::ToManyRequests
-      end
+    it 'regenerates the bucket upon first call' do
+      Redis.new.del instance.send :last_call_key
+      expect { subject }.not_to raise_error HttpError::ToManyRequests
+      expect(Redis.new.get(instance.send(:last_call_key))).to be_present
     end
   end
 end
